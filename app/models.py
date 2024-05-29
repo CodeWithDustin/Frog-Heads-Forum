c2ef6536b6a36b4076ce36671a7a9bb248abc87f
CodeWithDustin <codewithdustin@gmail.com>
2024-05-28 20:43:51 -0500

    Made Registrion and Login html templates

--- app/models.py
+++ app/models.py
@@ -5,40 +5,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_img = models.ImageField(
-        upload_to="profile_images", default="images/blank_image.jpg"
-    )
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=50)
+  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  profile_img = models.ImageField(upload_to='profile_images', default='images/blank_image.jpg')
+  bio = models.TextField(blank=True)
+  location = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.username
-
-
+  def __str__(self):
+    return self.username
+  
 class MessageBoard(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    date_created = models.DateTimeField(default=datetime.now)
+  title = models.CharField(max_length=100)
+  description = models.TextField(blank = True)
+  date_created = models.DateTimeField(default=datetime.now)
 
-    def __str__(self):
-        return self.title
+  def __str__(self):
+    return self.title
 
 
 class Post(models.Model):
-<<<<<<< HEAD
-    user = models.ForeignKey(User, on_delete=models.Case)
-    board = models.ForeignKey(MessageBoard, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100)
-    post = models.CharField(max_length=300)
-    date_posted = models.DateField(default=datetime.now)
-=======
   user = models.ForeignKey(User, on_delete=models.Case)
   board = models.ForeignKey(MessageBoard, on_delete=models.CASCADE)
   subject = models.CharField(max_length=100, blank=True, null=True)
   post = models.CharField(max_length=300)
   date_posted = models.DateField(default=datetime.now)
->>>>>>> 3b0e4934daecd4244c3aeeabbbf7a32fe18bc35a
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+  return self.name
+
+  
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from app.views import *
 
 urlpatterns = [
+    path('login/', login_view, name="login"),
+    path('register/', register_view, name='register'),
     path("", home_view, name="home"),
     path("admin/", admin.site.urls),
 ]
