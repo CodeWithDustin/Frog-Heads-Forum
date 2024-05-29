da118d8cbd50ff9230e7b0d30f25d332cfc26f7e
Nathan-T-Moss <nmoss2@basecampcodingacademy.org>
2024-05-28 20:17:04 -0500

    register, login, logout views filled out, need html files for the render

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 ### library imports ###
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group, User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 ### imports from other files in our project ###
+from app.models import Profile, MessageBoard, Post
+from app.forms import LoginForm
 
 
 ### Regular User Views ###
@@ -10,23 +15,66 @@ from django.shortcuts import render
 
 # user login
 def login_view(request):
-    pass
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            # user will be none if authentication fails
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                ## add error message here ##
+                pass
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "template name", context)
 
 
 # register user with Django's default user model
 def register_view(request):
-    pass
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            ## add user to group (need to create group in admin panel)##
+            user.save()
+            return redirect("login")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "template name", context)
 
 
 # user logout
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect("home")
 
 
 # user can see all forum categories, click on one to see all posts in that category
 # moderator could create/update/delete categories
 def home_view(request):
     return render(request, "home.html")
+
+
+# user can create profile, login required
+def create_profile_view(request):
+    pass
 
 
 # user can see account details, and post history
