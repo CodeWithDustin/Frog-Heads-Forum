929e3b231503a94e2501eef06c373e317cdc8a44
Nathan-T-Moss <nmoss2@basecampcodingacademy.org>
2024-05-30 12:29:36 -0500

    added related_name to profile

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  username = models.OneToOneField(User, related_name='profile' ,on_delete=models.CASCADE)
   profile_img = models.ImageField(upload_to='profile_images', default='images/blank_image.jpg')
   bio = models.TextField(blank=True)
   location = models.CharField(max_length=50)
